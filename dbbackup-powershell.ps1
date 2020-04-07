2e387cabb710c558f5e3637898be724f6fc60020
italo.cavalcante <italocavalcantefortaleza@live.com>
2020-04-07 15:12:40 -0300

    Archivelog backup finished

--- dbbackup-powershell.ps1
+++ dbbackup-powershell.ps1
@@ -1,18 +1,20 @@
 ﻿$OPERATION=$args[0]
-$ORACLE_SID="ORCLNOVO"
+$env:ORACLE_SID
 $RMAN_BACKUP_HOME_DIR="C:\backup"
 $RMAN_FILE_NAME=$env:TEMP+"\rman-arc.rman"
-$RMAN_FILE_LOG=$RMAN_BACKUP_HOME_DIR+"\$ORACLE_SID\LOGS\"
+$RMAN_FILE_LOG=$RMAN_BACKUP_HOME_DIR+"\$ORACLE_SID\LOGS"
+$RMAN_ARC_PATH=$RMAN_BACKUP_HOME_DIR+"\$ORACLE_SID\rman\arc"
 
+Write-Output $RMAN_ARC_PATH
 Function print_usage{
-  echo "Usage:"
-  echo "  dbbackup-powershell.ps1 --level0"
-  echo "  dbbackup-powershell.ps1 --level1"
-  echo "  dbbackup-powershell.ps1 --arc"
-  echo "  dbbackup-powershell.ps1 --expdp"
-  echo "  dbbackup-powershell.ps1 --configure"
-  echo "  dbbackup-powershell.ps1 --crosscheck"
-  echo "  dbbackup-powershell.ps1 --validate"
+  Write-Output "Usage:"
+  Write-Output "  dbbackup-powershell.ps1 --level0"
+  Write-Output "  dbbackup-powershell.ps1 --level1"
+  Write-Output "  dbbackup-powershell.ps1 --arc"
+  Write-Output "  dbbackup-powershell.ps1 --expdp"
+  Write-Output "  dbbackup-powershell.ps1 --configure"
+  Write-Output "  dbbackup-powershell.ps1 --crosscheck"
+  Write-Output "  dbbackup-powershell.ps1 --validate"
 }
 
 switch ($operation)
@@ -21,15 +23,16 @@ switch ($operation)
     "--level1" {"Starting backup Level1"}
     "--arc" {"Starting backup Archivelog";
     $RMAN_FILE_LOG=$RMAN_BACKUP_HOME_DIR+"\$ORACLE_SID\LOGS\arc-"+$(Get-Date -Format "ddMMyyyy-HHmmss")+".log";
-    $RMAN_FILE_LOG
-                echo $env:TEMP
-                echo "run { " > $RMAN_FILE_NAME
-                echo "     ALLOCATE CHANNEL C1 TYPE DISK MAXPIECESIZE 8G;" >> $RMAN_FILE_NAME
-                echo "     DELETE NOPROMPT ARCHIVELOG ALL BACKED UP 1 TIMES TO DEVICE TYPE DISK COMPLETED BEFORE 'SYSDATE-36/24'; " >> $RMAN_FILE_NAME
-                echo "     BACKUP ARCHIVELOG ALL  FORMAT '$RMAN_ARC_PATH/arc_%T_set%s_piece%p_copy%c_%t.bkp' FILESPERSET 64; " >> $RMAN_FILE_NAME
-                echo "     RELEASE CHANNEL C1;" >> $RMAN_FILE_NAME
-                echo " }" >> $RMAN_FILE_NAME
-                rm $RMAN_FILE_NAME
+   
+                Write-Output "run { " > $RMAN_FILE_NAME
+                Write-Output "     ALLOCATE CHANNEL C1 TYPE DISK MAXPIECESIZE 8G;" >> $RMAN_FILE_NAME
+                Write-Output "     DELETE NOPROMPT ARCHIVELOG ALL BACKED UP 1 TIMES TO DEVICE TYPE DISK COMPLETED BEFORE 'SYSDATE-36/24'; " >> $RMAN_FILE_NAME
+                Write-Output "     BACKUP ARCHIVELOG ALL  FORMAT '$RMAN_ARC_PATH/arc_%T_set%s_piece%p_copy%c_%t.bkp' FILESPERSET 64; " >> $RMAN_FILE_NAME
+                Write-Output "     RELEASE CHANNEL C1;" >> $RMAN_FILE_NAME
+                Write-Output " }" >> $RMAN_FILE_NAME
+                Write-Output $RMAN_FILE_NAME
+                        type $RMAN_FILE_NAME | rman target /
+               
           }
     "--crosscheck" {"Starting backup crosscheck"}
     Default {
